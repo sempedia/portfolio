f680b0e02bfbf309cfa9b4cb192ed3f4f310d8b4
Alina <sempedia@gmail.com>
2023-08-18 01:57:05 +0300

    wsgi update

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -6,16 +6,20 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
-# HEROKU DEPLOYMENT
+# Render deployment
 application = get_wsgi_application()
+application = WhiteNoise(application)
 
+
+# HEROKU DEPLOYMENT
+# application = get_wsgi_application()
 
 # VERCEL DEPLOYMENT
-app = application
+# app = application
+
